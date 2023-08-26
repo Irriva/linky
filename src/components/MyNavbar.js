6081af4828c590c92bf01142fcf788fdecb5d9ff
Irriva <115064063+Irriva@users.noreply.github.com>
2023-08-26 10:26:23 +0530

    upda
    a

--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -12,7 +12,7 @@ export class MyNavbar extends Component {
       <div>
         <Navbar expand="lg" className="bg-body-tertiary">
           <Container>
-            <Navbar.Brand href="/">Newsily</Navbar.Brand>
+            <Navbar.Brand href="/">newsily</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
